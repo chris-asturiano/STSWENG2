78ac729bacc9724c2e7f2381de4c5592f8ddb474
chris <christianast14@gmail.com>
2024-04-04 18:39:32 +0800

    switched back to the old db for now, tests seem to be working as expected

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const Pet = require('./schemas/Pet')
 
 // Connect to MongoDB
 mongoose
-    .connect('mongodb+srv://user1:asd12345@swengtest.svfsh4k.mongodb.net/STSWENG2?retryWrites=true&w=majority&appName=swengtest')
+    .connect('mongodb://127.0.0.1:27017/STSWENG')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB', err));
 
