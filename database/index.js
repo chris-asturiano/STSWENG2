1ae23b0ed50c060b869d515600c79ebd158a562e
carl-yap <87176222+carl-yap@users.noreply.github.com>
2024-04-06 01:20:35 +0800

    search working!!!!
    currently refreshes the page every time it searches, improvement can be to use AJAX

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const Pet = require('./schemas/Pet')
 
 // Connect to MongoDB
 mongoose
-    .connect('mongodb+srv://user1:asd12345@swengtest.svfsh4k.mongodb.net/ancient-pet-exhibit?retryWrites=true&w=majority&appName=swengtest')
+    .connect('mongodb+srv://user1:asd12345@swengtest.svfsh4k.mongodb.net/STSWENG2?retryWrites=true&w=majority&appName=swengtest')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB', err));
 
@@ -44,8 +44,37 @@ async function closeConnection() {
   mongoose.connection.close();
 }
 
+async function pet_populate(species, petcode, name, description, in_gallery, affection, temper) {
+  try {
+      let entry = new Pet({ species: species, petcode: petcode, name: name, description: description, in_gallery: in_gallery, affection: affection, temper: temper})
+      await entry.save()
+      console.log("Pet successfully created.")
+  } catch (e) {
+      console.log(e.message)
+  }
+}
+
+async function pet_populate_attr(petcode, maintenance, temper) {
+  try {
+    const entry = await Pet.findOne({petcode: petcode});
+
+    if (entry){
+      entry.maintenance = maintenance;
+      entry.temper = temper;
+      await entry.save()
+      console.log("Pet successfully updated.")
+    }  else  {
+      console.log("Pet does not exist :(((((")
+    }
+  } catch (e) {
+      console.log(e.message)
+  }
+}
+
 module.exports = {
   userPopulate,
   usersDelete,
   closeConnection,
+  pet_populate,
+  pet_populate_attr,
 };
