be4685a2230f8f6722304ea8f8c13fc1905d2c58
chris <117173407+chris-asturiano@users.noreply.github.com>
2024-04-07 21:45:55 +0800

    Update login_route.test.js

--- tests/login_route.test.js
+++ tests/login_route.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const User = require('../database/schemas/User');
+const bcrypt = require('bcrypt');
 const app = require('../app').app;
 
 const { closeConnection } = require('../database');
@@ -8,9 +9,12 @@ const { closeConnection } = require('../database');
 
 describe('POST /login', () => {
   it('responds with redirect to /profiles on successful login', async () => {
+    // hashed password
+    const hashedPassword = await bcrypt.hash('password', 10);
+
     const fakeUser = { username: 'testuser', password: 'password'};
     
-    await User.create({ username: 'testuser', password: 'password', email: 'testemail@gmail.com', role: 'adopter' });
+    await User.create({ username: 'testuser', password: hashedPassword, email: 'testemail@gmail.com', role: 'adopter' });
 
     const response = await request(app)
       .post('/login_route/login')
