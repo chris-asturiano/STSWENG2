1beb912ce4ed591877a669dabb0c3f71e05c9fdf
chris <christianast14@gmail.com>
2024-04-07 21:47:02 +0800

    added password hashing

--- routes/login_route.js
+++ routes/login_route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../database/schemas/User');
 
 // Also require like database stuffs for MongoDB, put here
@@ -14,57 +15,51 @@ router.get('/', async (req, res)=>{
 	}
 });
 
-router.post('/login', async function(req, res){// /login_route/login
-	try {
-		//logic for checkin if user is adopter/kennel
-		const login_user = await User.findOne({ username: req.body.username });
-		if(login_user){
-			const result = req.body.password === login_user.password;
-			if (result){
-				console.log('Login Success');
-				req.session.userId = login_user._id;
-				req.session.search = get_empty_search();
-				res.redirect('/profiles');
-			} else {
-				console.log('Login Fail');
-				return res.render('login', { error: 'Invalid password', title: 'Login Page', has_style: 'login_style' });
-			}
-		} else {
-			console.log('Login Fail');
-			return res.render('login', { error: 'Invalid username', title: 'Login Page', has_style: 'login_style' });
-		}
-
-		// for now all reroutes to pet search, but pet search is for adopter
-		console.log('Login Attempted');
-		// res.redirect('/petSearch.html');
-		// else go back, than error????
-	} catch (err) {
-		console.error('Error fetching user:', err);
-		res.status(500).send('Internal Server Error');
-	}
+router.post('/login', async function(req, res) {
+    try {
+        // Find the user by username
+        const login_user = await User.findOne({ username: req.body.username });
+        if (login_user) {
+            const result = await bcrypt.compare(req.body.password, login_user.password);
+            if (result) {
+                console.log('Login Success');
+                req.session.userId = login_user._id;
+                req.session.search = get_empty_search();
+                res.redirect('/profiles');
+            } else {
+                console.log('Login Fail');
+                return res.render('login', { error: 'Invalid password', title: 'Login Page', has_style: 'login_style' });
+            }
+        } else {
+            console.log('Login Fail');
+            return res.render('login', { error: 'Invalid username', title: 'Login Page', has_style: 'login_style' });
+        }
+    } catch (err) {
+        console.error('Error fetching user:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 function get_empty_search() {
-	return { 
-		species: {
-			DNM: false,
-			doggo: false,
-			hissy: false,
-			borb: false,
-			smol: false
-		}, 
-		maintenance: {
-			DNM: false,
-			high: false,
-			low: false
-		}, 
-		temper: {
-			DNM: false,
-			playful: false,
-			lax: false
-		} 
-	};
+    return { 
+        species: {
+            DNM: false,
+            doggo: false,
+            hissy: false,
+            borb: false,
+            smol: false
+        }, 
+        maintenance: {
+            DNM: false,
+            high: false,
+            low: false
+        }, 
+        temper: {
+            DNM: false,
+            playful: false,
+            lax: false
+        } 
+    };
 }
-
 
 module.exports = router;
